63d1fc84c5649b7a6f4c4443c26e70048944a987
matthew pickett <pic11005@byui.edu>
2019-07-13 00:28:00 -0600

    added db and login

---
+++ controllers/userController.js
@@ -0,0 +1,69 @@
+const fetch = require('node-fetch'); //for API request
+
+const { Pool } = require('pg');
+const db_url = process.env.DATABASE_URL;
+const pool = new Pool({connectionString: db_url});
+
+
+function signup(req, res){
+    console.log("signup page requested");
+
+    const username = req.body.username;
+    const password = req.body.password;
+
+    var sql = "insert into users (username, password) values($1, $2)";
+
+    pool.query(sql, function(err, data) {
+        if(err) {
+            console.log(err)
+        } else {
+
+            console.log("Added to database" + data);
+            res.json(data);
+        };
+    });
+};
+
+function handleLogin(req, res) {
+    console.log("Made it to the handle Login function");
+    var result = {success: false};
+
+    if(req.body.username == 'admin' && req.body.password =='password') {
+        req.session.user = req.body.username;
+
+        result= {success: true};
+    }
+    res.json(result);
+}
+
+function callReddit(req, res) {
+    
+    const searchTerm = req.query.searchTerm;
+
+    fetch('https://api.reddit.com/r/' + searchTerm + '/top.json?sort=rising&t=day&limit=4')
+
+    .then(res => res.json())
+    .then(function(myJson){
+    console.log(myJson.data.children[0].data.title);
+
+    const data = myJson.data.children[0].data.title;
+    const data1 = myJson.data.children[1].data.title;
+    const data2 = myJson.data.children[2].data.title;
+    const data3 = myJson.data.children[3].data.title;
+
+    const posts = {
+        data:data,
+        data1:data1,
+        data2:data2, 
+        data3:data3
+    }
+
+    res.render('results', posts)
+    })
+}
+
+module.exports = {
+    signup: signup, 
+    handleLogin: handleLogin, 
+    callReddit: callReddit
+}
--- index.js
+++ index.js
@@ -7,10 +7,7 @@ const http = require('http');
 const fetch = require('node-fetch');
 var session = require('express-session')
 var path = require('path');
-const { Pool } = require('pg');
-
-const connectionString = process.env.DATABASE_URL || "";
-const pool = new Pool({connectionString:connectionString});
+const userController = require('./controllers/userController.js');
 
 app.use(express.json({limit: '1mb'}))
 app.use(express.static(path.join(__dirname, 'public')))
@@ -19,62 +16,16 @@ app.use(express.urlencoded({ extended: true }));
 app.set("views", "views");
 app.set('view engine', 'ejs');
 
-app.use(session({
-  secret: 'Mmpjordan23',
-  resave: false,
-  saveUninitialized: true
-}))
-
 app.listen(port, function() {
   console.log('Node app is running on port', port);
 });
 
+app.get("/reddit", userController.callReddit);
+
+app.post('/login', userController.handleLogin);
+app.post('/signup', userController.signup);
 
 
-app.get("/reddit", (req, res) => {
-
-  const searchTerm = req.query.searchTerm;
-
-  fetch('https://api.reddit.com/r/' + searchTerm + '/top.json?sort=rising&t=day&limit=4')
-
-  .then(res => res.json())
-  .then(function(myJson){
-    console.log(myJson.data.children[0].data.title);
-
-    const data = myJson.data.children[0].data.title;
-    const data1 = myJson.data.children[1].data.title;
-    const data2 = myJson.data.children[2].data.title;
-    const data3 = myJson.data.children[3].data.title;
-
-    const posts = {
-      data:data,
-      data1:data1,
-      data2:data2, 
-      data3:data3
-    }
-
-    res.render('results', posts)
-  })
-});
-
-
-
-app.post('/login', handleLogin);
-
-//processes the js script in public folder
-function handleLogin(req, res) {
-  console.log("Made it to the handle Login function");
-  var result = {success: false};
-
-  if(req.body.username == 'admin' && req.body.password =='password') {
-    req.session.user = req.body.username;
-
-    result= {success: true};
-  }
-
-  res.json(result);
-
-}
 
 
 
